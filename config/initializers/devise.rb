f068f04702b0ff88b372c898be8a21b29840e973
Robert Morgan <robbie.morgan@gmail.com>
2015-07-22 15:03:33 +0100

    Facebook login working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-  config.omniauth :facebook, "1603963326557862", ENV["FACEBOOK_YELP_APP_SECRET"]
+  config.omniauth :facebook, "1603963326557862", ENV["FACEBOOK_YELP_APP_SECRET"], info_fields: 'email'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
